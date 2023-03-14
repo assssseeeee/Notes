2fc5bb5d663057e0943c62ad7e27b61421510a3e
klementev <assssseeeee@gmail.com>
2023-03-14 20:45:24 +0300

    create room database

--- app/src/main/java/com/example/notes/screens/MainScreen.kt
+++ app/src/main/java/com/example/notes/screens/MainScreen.kt
@@ -1,31 +1,42 @@
 package com.example.notes.screens
 
 import android.annotation.SuppressLint
+import android.app.Application
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.notes.model.Note
 import com.example.notes.navigation.NavRoute
+import com.example.notes.ui.MainViewModel
+import com.example.notes.ui.MainViewModelFactory
 import com.example.notes.ui.theme.NotesTheme
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen(navController: NavHostController) {
-
+    val context = LocalContext.current
+    val viewModel: MainViewModel =
+        viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
+    val notes = viewModel.readTest.observeAsState(listOf()).value
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
@@ -40,18 +51,16 @@ fun MainScreen(navController: NavHostController) {
             }
         }
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            NoteItem(title = "Title 1", subTitle = "Subtitle 1", navController = navController)
-            NoteItem(title = "Title 2", subTitle = "Subtitle 2", navController = navController)
-            NoteItem(title = "Title 3", subTitle = "Subtitle 3", navController = navController)
-            NoteItem(title = "Title 4", subTitle = "Subtitle 4", navController = navController)
+        LazyColumn {
+            items(notes) { note ->
+                NoteItem(note = note, navController = navController)
+            }
         }
-
     }
 }
 
 @Composable
-fun NoteItem(title: String, subTitle: String, navController: NavHostController) {
+fun NoteItem(note: Note, navController: NavHostController) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -68,12 +77,12 @@ fun NoteItem(title: String, subTitle: String, navController: NavHostController) 
         ) {
 
             Text(
-                text = title,
+                text = note.title,
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold
             )
             Text(
-                text = subTitle
+                text = note.subtitle
             )
         }
 
--- app/src/main/java/com/example/notes/screens/StartScreen.kt
+++ app/src/main/java/com/example/notes/screens/StartScreen.kt
@@ -1,5 +1,7 @@
 package com.example.notes.screens
 
+import android.annotation.SuppressLint
+import android.app.Application
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
@@ -7,18 +9,28 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.notes.navigation.NavRoute
+import com.example.notes.ui.MainViewModel
+import com.example.notes.ui.MainViewModelFactory
 import com.example.notes.ui.theme.NotesTheme
+import com.example.notes.utils.TYPE_FIREBASE
+import com.example.notes.utils.TYPE_ROOM
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun StartScreen(navController: NavHostController) {
+    val context = LocalContext.current
+    val viewModel: MainViewModel =
+        viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-    ) { padding ->
+    ) {
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -26,7 +38,10 @@ fun StartScreen(navController: NavHostController) {
         ) {
             Text(text = "What will we use?")
             Button(
-                onClick = { navController.navigate(route = NavRoute.Main.route) },
+                onClick = {
+                    viewModel.initDatabase(TYPE_ROOM)
+                    navController.navigate(route = NavRoute.Main.route)
+                },
                 modifier = Modifier
                     .width(200.dp)
                     .padding(vertical = 8.dp)
@@ -34,7 +49,10 @@ fun StartScreen(navController: NavHostController) {
                 Text(text = "Room database")
             }
             Button(
-                onClick = { navController.navigate(route = NavRoute.Main.route) },
+                onClick = {
+                    viewModel.initDatabase(TYPE_FIREBASE)
+                    navController.navigate(route = NavRoute.Main.route)
+                },
                 modifier = Modifier
                     .width(200.dp)
                     .padding(vertical = 8.dp)
